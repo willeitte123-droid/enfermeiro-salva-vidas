8112a868208f26d09a51a1fcbf8d215dc11b0e8c
[dyad] <willeitte123@gmail.com>
2025-10-14 23:16:29 -0500

    [dyad] Corrigindo definitivamente o erro de tela branca na navegação - wrote 1 file(s)

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -47,9 +47,7 @@ const MainLayout = ({ session, profile }: MainLayoutProps) => {
         <Header onSearchClick={() => setIsSearchOpen(true)} isAdmin={isAdmin} user={user} />
         <GlobalSearch open={isSearchOpen} setOpen={setIsSearchOpen} />
         <div className="flex-1 overflow-y-auto p-4 sm:p-6 md:p-8">
-          <Suspense fallback={<LoadingFallback />}>
-            <Outlet context={{ profile }} />
-          </Suspense>
+          <Outlet context={{ profile }} />
         </div>
       </main>
     </div>
