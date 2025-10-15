2fa1ad15751d290392470e1a55764af232b213ad
[dyad] <willeitte123@gmail.com>
2025-10-14 19:56:08 -0500

    [dyad] Corrigindo erro de navegação com StrictMode - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
