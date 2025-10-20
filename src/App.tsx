1d2d832a97285128f9b703f339ee379b0b392943
[dyad] <willeitte123@gmail.com>
2025-10-20 18:02:42 -0500

    [dyad] Corrigindo erro de importação de layout - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
-import RootLayout from "./layouts/RootLayout";
-import AuthLayout from "./layouts/AuthLayout";
-import AdminLayout from "./layouts/AdminLayout";
+import RootLayout from "@/layouts/RootLayout";
+import AuthLayout from "@/layouts/AuthLayout";
+import AdminLayout from "@/layouts/AdminLayout";
 import ErrorPage from "@/pages/ErrorPage";
 import LoginPage from "@/pages/LoginPage";
 import SignUpPage from "@/pages/SignUpPage";
@@ -83,7 +83,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster richColors />
-    </QueryClientProvider>
+    </Query-ClientProvider>
   );
 }
 
