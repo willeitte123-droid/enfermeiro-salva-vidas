198bb608a4de181328ac474786dea2eafe54f470
[dyad] <willeitte123@gmail.com>
2026-02-24 06:31:39 -0500

    Reverted all changes back to version a41499d4b756e9fc6ffbe86b2d14511190df5dad

--- supabase/migrations/20240702_unify_fundamentals_flashcards.sql
+++
@@ -1,4 +0,0 @@
--- Atualiza todos os flashcards da categoria antiga para a nova nomenclatura padronizada
-UPDATE public.flashcards
-SET deck_category = 'Fundamentos de Enfermagem'
-WHERE deck_category = 'Fundamentos';
